11639d219401409051f6ae8b7de3bf7ae21f6b0f
Kat Zdzienkowska <k.zdzienkowska@gmail.com>
2022-08-10 16:37:10 +0100

    room added and tested + planning diagram

--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,4 +1,54 @@
 package Rooms;
+import Enums.Enemy;
+import Enums.Treasure;
+
+import java.util.ArrayList;
 
 public class Room {
+
+    private String name;
+    private ArrayList<Treasure> treasures;
+    private ArrayList<Enemy> enemies;
+
+    public Room(String name) {
+        this.name = name;
+        this.treasures = new ArrayList<>();
+        this.enemies = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Treasure> getTreasures() {
+        return treasures;
+    }
+
+    public ArrayList<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public void addTreasure(Treasure treasure) {
+        this.treasures.add(treasure);
+    }
+
+    public void addEnemy(Enemy enemy) {
+        this.enemies.add(enemy);
+    }
+
+    public void removeTreasure(Treasure treasure) {
+        this.treasures.remove(treasure);
+    }
+
+    public void removeEnemy(Enemy enemy) {
+        this.enemies.remove(enemy);
+    }
+
+    public int countTheTreasures() {
+        return this.treasures.size();
+    }
+
+    public int countTheEnemies() {
+        return this.enemies.size();
+    }
 }
--- src/test/java/RoomTest.java
+++ src/test/java/RoomTest.java
@@ -1,2 +1,70 @@
+import Rooms.Room;
+import Enums.Treasure;
+import Enums.Enemy;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class RoomTest {
+
+    Room swamp;
+
+
+    @Before
+    public void before() {
+        swamp = new Room("Swamp");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Swamp", swamp.getName());
+    }
+
+    @Test
+    public void canGetTreasures() {
+        assertEquals(0, swamp.getTreasures().size());
+    }
+
+    @Test
+    public void canGetEnemies() {
+        assertEquals(0, swamp.getEnemies().size());
+    }
+
+    @Test
+    public void canAddTreasuresToRoom() {
+        assertEquals(0, swamp.countTheTreasures());
+        swamp.addTreasure(Treasure.GOLD);
+        assertEquals(1, swamp.countTheTreasures());
+    }
+
+    @Test
+    public void canAddEnemiesToRoom() {
+        assertEquals(0, swamp.countTheEnemies());
+        swamp.addEnemy(Enemy.TROLL);
+        assertEquals(1, swamp.countTheEnemies());
+    }
+
+    @Test
+    public void canRemoveTreasuresToRoom() {
+        assertEquals(0, swamp.countTheTreasures());
+        swamp.addTreasure(Treasure.GOLD);
+        swamp.addTreasure(Treasure.GEMS);
+        assertEquals(2, swamp.countTheTreasures());
+        swamp.removeTreasure(Treasure.GOLD);
+        assertEquals(1, swamp.countTheTreasures());
+    }
+
+    @Test
+    public void canRemoveEnemiesToRoom() {
+        assertEquals(0, swamp.countTheEnemies());
+        swamp.addEnemy(Enemy.TROLL);
+        swamp.addEnemy(Enemy.ORC);
+        assertEquals(2, swamp.countTheEnemies());
+        swamp.removeEnemy(Enemy.TROLL);
+        assertEquals(1, swamp.countTheEnemies());
+    }
+
 }
